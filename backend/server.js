7f5d95da3765dd2250ead1e30d549d119f55dd9c
temi <icodetenmp@gmail.com>
2025-11-28 08:48:57 -0800

    Use better-sqlite3 + Render fixes

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,9 @@ app.get('/',(req, res) =>{
 } );
 
 //start server
-const port = process.env.PORT || 5000;
-app.listen(port, ( )=> {
-console.log(`server runing on port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ( )=> {
+console.log(`server runing on port ${PORT}`);
  
 });
  
