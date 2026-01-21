175981564687f199b883d8da5abe6923a6bb5807
temi <icodetenmp@gmail.com>
2026-01-21 00:21:24 -0800

    fix update4

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 // server.js
+
 require("dotenv").config();
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options("*", cors());
+
+
 
 require("./seedTracks");
 
