aa5efae8d84053e274ca5cc1e38563cbd9289ec3
temi <icodetenmp@gmail.com>
2026-01-30 12:51:30 -0800

    fix update7-today5.5

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-    origin: [
-        'http://127.0.0.1:5500',
-        'http://localhost:5500',
-        'https://my-music-appp.onrender.com'
-    ],
-    methods: ["GET", "PUT"],
-    allowedHeaders: ["Content-Type"],
-}));
+app.use(cors());
 
-app.options('*', cors());
 
 
 require("./seedTracks");
 
 const tracksRouter = require('./routes/tracks');
+
 
 
 // Parse JSON bodies
